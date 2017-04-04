2a250402714faf435962832865f4166d92edb916
Paul Farino <paul.farino1@gmail.com>
2017-04-03 21:19:03 -0700

    Clean config +  FTP deploy option

--- config.rb
+++ config.rb
@@ -1,22 +1,25 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :dotenv
+activate :sprockets
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
-activate :automatic_alt_tags
-activate :autoprefixer
-activate :directory_indexes
-activate :sprockets
-activate :autoprefixer
-
-configure :development do
-  activate :livereload
-end
-
 configure :build do
   activate :minify_css
   activate :minify_javascript
+end
+
+
+# Deploy to FTP
+activate :deploy do |deploy|
+  deploy.deploy_method   = :ftp
+  deploy.host     = ENV['FTP_URL']
+  deploy.path     = ENV['FTP_PATH']
+  deploy.user     = ENV['FTP_USERNAME']
+  deploy.password = ENV['FTP_PASSWORD']
 end
